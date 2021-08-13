11555ac50de1809842c8846f1b2617dfad995125
Alex Watts <alexjwatts83@gmail.com>
2021-08-13 11:44:34 +1000

    Added log

--- src/MediatRDemo.Application/Handlers/Notifier1.cs
+++ src/MediatRDemo.Application/Handlers/Notifier1.cs
@@ -3,23 +3,38 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using MediatRDemo.Application.Notifcations;
+using Microsoft.Extensions.Logging;
 
 namespace MediatRDemo.Application.Handlers
 {
     public class Notifier1 : INotificationHandler<NotificationMessage>
     {
+        private readonly ILogger<Notifier1> _logger;
+
+        public Notifier1(ILogger<Notifier1> logger)
+        {
+            _logger = logger;
+        }
+
         public Task Handle(NotificationMessage notification, CancellationToken cancellationToken)
         {
-            Debug.WriteLine($"Debugging from Notifier 1. Message  : {notification.NotifyText} ");
+            _logger.LogInformation($"Debugging from Notifier 1. Message  : {notification.NotifyText} ");
             return Task.CompletedTask;
         }
     }
 
     public class Notifier2 : INotificationHandler<NotificationMessage>
     {
+        private readonly ILogger<Notifier1> _logger;
+
+        public Notifier2(ILogger<Notifier1> logger)
+        {
+            _logger = logger;
+        }
+
         public Task Handle(NotificationMessage notification, CancellationToken cancellationToken)
         {
-            Debug.WriteLine($"Debugging from Notifier 2. Message  : {notification.NotifyText} ");
+            _logger.LogInformation($"Debugging from Notifier 2. Message  : {notification.NotifyText} ");
             return Task.CompletedTask;
         }
     }
