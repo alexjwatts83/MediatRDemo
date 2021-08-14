fb52c1104ec8b3654949cef6a0e8401fef76e438
Alex Watts <alexjwatts83@gmail.com>
2021-08-15 09:48:59 +1000

    twelve week controller inherits from base controller

---
+++ src/MediatRDemo.WebApi/Controllers/GoalController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using MediatRDemo.Application.Goals.Queries;
+using MediatRDemo.Application.TwelveWeekYears.Queries;
+using MediatRDemo.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MediatRDemo.WebApi.Controllers
+{
+	public class GoalController : BaseApiController<Goal, Guid, GetAllGoalsQuery, GetByIdGoalQuery>
+	{
+		public GoalController(IMediator mediator) : base(mediator)
+		{
+		}
+	}
+}
--- src/MediatRDemo.WebApi/Controllers/TwelveWeekYearController.cs
+++ src/MediatRDemo.WebApi/Controllers/TwelveWeekYearController.cs
@@ -1,44 +1,12 @@
 ﻿using MediatR;
-using MediatRDemo.Application.Goals.Queries;
 using MediatRDemo.Application.TwelveWeekYears.Queries;
 using MediatRDemo.Domain.Entities;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace MediatRDemo.WebApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class TwelveWeekYearController : ControllerBase
+	public class TwelveWeekYearController : BaseApiController<TwelveWeekYear, int, GetAllTwelveWeekYearQuery, GetByIdTwelveWeekYearQuery>
 	{
-		private readonly IMediator _mediator;
-		public TwelveWeekYearController(IMediator mediator)
-		{
-			_mediator = mediator;
-		}
-
-		// GET: api/twelve-week-year
-		[HttpGet]
-		public async Task<ActionResult<IReadOnlyList<TwelveWeekYear>>> GetAllAsync()
-		{
-			var list = await _mediator.Send(new GetAllTwelveWeekYearQuery());
-			return Ok(list);
-		}
-
-		// GET: api/twelve-week-year/{id}
-		[HttpGet("{id}")]
-		public async Task<ActionResult<TwelveWeekYear>> GetByIdAsync(int id)
-		{
-			var request = new GetByIdTwelveWeekYearQuery() { Id = id };
-			var list = await _mediator.Send(request);
-			return Ok(list);
-		}
-	}
-	public class GoalController : BaseApiController<Goal, Guid, GetAllGoalsQuery, GetByIdGoalQuery>
-	{
-		public GoalController(IMediator mediator) : base(mediator)
+		public TwelveWeekYearController(IMediator mediator) : base(mediator)
 		{
 		}
 	}
