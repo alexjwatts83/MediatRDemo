f319a880bd4da23e18c94e3b74adf1ca065cccde
Alex Watts <alexjwatts83@gmail.com>
2021-08-15 09:51:31 +1000

    deleted unused code

--- src/MediatRDemo.Application/Notifcations/NotificationMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace MediatRDemo.Application.Notifcations
-{
-    public class NotificationMessage : INotification
-    {
-        public string NotifyText { get; set; }
-    }
-}
