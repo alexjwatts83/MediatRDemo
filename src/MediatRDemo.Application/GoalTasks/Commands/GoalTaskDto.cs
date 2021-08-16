81539d4ffbb0d650cfcd445e8ab10dd6c11bbf40
Alex Watts <alexjwatts83@gmail.com>
2021-08-17 07:28:45 +1000

    Adde goaltaskdto

---
+++ src/MediatRDemo.Application/GoalTasks/Commands/GoalTaskDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatRDemo.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using MediatRDemo.Application.Common.Mappings;
+
+namespace MediatRDemo.Application.Goals.Commands
+{
+	public class GoalTaskDto : IMapFrom<GoalTask>
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		//public IEnumerable<GoalTask> Tasks { get; set; } = Enumerable.Empty<GoalTask>();
+	}
+}
