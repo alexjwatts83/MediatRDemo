92254e7277548921aefaf8023a53109aa5bf6bff
Alex Watts <alexjwatts83@gmail.com>
2021-08-16 21:37:11 +1000

    added db health check

--- src/MediatRDemo.WebApi/HealthChecks/DbHeathCheck.cs
+++ src/MediatRDemo.WebApi/HealthChecks/DbHeathCheck.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+﻿using MediatRDemo.Infrastructure.Persistence.Configurations;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,9 +10,35 @@ namespace MediatRDemo.WebApi.HealthChecks
 {
 	public class DbHeathCheck : IHealthCheck
 	{
-		public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+		private readonly string _connectionString;
+		private readonly string _testSqlQuery;
+
+		public DbHeathCheck(IOptions<ConnectionStringSettings> connectionStrings)
 		{
-			throw new NotImplementedException();
+			_connectionString = connectionStrings.Value.Database;
+			_testSqlQuery = "EXEC [dbo].[HealthCheck]";
+		}
+		public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+		{
+			using (var connection = new SqlConnection(_connectionString))
+			{
+				try
+				{
+					await connection.OpenAsync(cancellationToken);
+
+					using (var command = connection.CreateCommand())
+					{
+						command.CommandText = _testSqlQuery;
+						await command.ExecuteScalarAsync(cancellationToken);
+					}
+
+					return HealthCheckResult.Healthy("Blah Blah");
+				}
+				catch (Exception ex)
+				{
+					return HealthCheckResult.Unhealthy(exception: ex);
+				}
+			}
 		}
 	}
 }
