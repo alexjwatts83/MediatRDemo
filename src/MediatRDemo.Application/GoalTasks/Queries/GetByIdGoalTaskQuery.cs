567858046933575ab593129e7bb0f6287a9908ba
Alex Watts <alexjwatts83@gmail.com>
2021-08-17 07:26:13 +1000

    Added GetByIdGoalTaskQuery

---
+++ src/MediatRDemo.Application/GoalTasks/Queries/GetByIdGoalTaskQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MediatRDemo.Application.Base.Queries;
+using MediatRDemo.Application.Interfaces;
+using MediatRDemo.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatRDemo.Application.GoalTasks.Queries
+{
+	public class GetByIdGoalTaskQuery : GetByIdBaseQuery<GoalTask, Guid>
+	{
+	}
+
+	public class GetByIdGoalTaskQueryHandler
+		: GetByIdBaseQueryHandler<GoalTask, Guid>
+		, IRequestHandler<GetByIdGoalTaskQuery, GoalTask>
+	{
+		public GetByIdGoalTaskQueryHandler(IUnitOfWork unitOfWork, IGoalTasksGenericCrudRepositoryScripts scripts)
+			: base(unitOfWork, scripts)
+		{
+		}
+
+		public Task<GoalTask> Handle(GetByIdGoalTaskQuery request, CancellationToken cancellationToken)
+			=> base.Handle(request, cancellationToken);
+	}
+}
