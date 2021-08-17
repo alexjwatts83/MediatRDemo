2c3f45289b6c3149ed99239d7aa63f08e070e403
Alex Watts <alexjwatts83@gmail.com>
2021-08-18 08:04:02 +1000

    fixed up ValidationBehavior

--- src/MediatRDemo.Application/Common/Behaviours/ValidationBehavior.cs
+++ src/MediatRDemo.Application/Common/Behaviours/ValidationBehavior.cs
@@ -24,35 +24,16 @@ namespace MediatRDemo.Application.Common.Behaviours
 		{
 			if (_validators.Any())
 			{
+				var context = new FluentValidation.ValidationContext<TRequest>(request);
 
+				var validationResults = await Task.WhenAll(_validators.Select(v =>v.ValidateAsync(context, cancellationToken)));
+
+				var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+				if (failures.Count != 0)
+					throw new ValidationException(failures);
 			}
 			return await next();
 		}
-
-		//public async Task<TResponse> Handle(TRequest request,
-		//	CancellationToken cancellationToken,
-		//	RequestHandlerDelegate<TResponse> next)
-		//{
-		//	if (_validators.Any())
-		//	{
-		//		var context = new ValidationContext(request);
-
-		//	//	var validationResults = await Task
-		//	//		.WhenAll(_validators.Select(v =>
-		//	//			v.ValidateAsync(context, cancellationToken)));
-
-		//	//	var failures = validationResults
-		//	//		.SelectMany(r => r.Errors)
-		//	//		.Where(f => f != null)
-		//	//		.ToList();
-
-		//	//	if (failures.Count != 0)
-		//	//	{
-		//	//		throw new ValidationException(failures);
-		//	//	}
-		//	//}
-
-		//	return await next();
-		//}
 	}
 }
