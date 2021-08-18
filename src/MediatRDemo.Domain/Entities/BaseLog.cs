687a232cabda5de068bdaf7f83fa71cda301d877
Alex Watts <alexjwatts83@gmail.com>
2021-08-19 07:20:13 +1000

    Added more entities

---
+++ src/MediatRDemo.Domain/Entities/BaseLog.cs
@@ -0,0 +1,8 @@
+﻿namespace MediatRDemo.Domain.Entities
+{
+	public abstract class BaseLog : BaseEntity<int>
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ src/MediatRDemo.Domain/Entities/LogType.cs
@@ -0,0 +1,8 @@
+﻿namespace MediatRDemo.Domain.Entities
+{
+	public class LogType : BaseEntity<int>
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
