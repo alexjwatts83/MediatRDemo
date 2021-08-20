0e21ce0cbc917fbac8a40fb56f0280d616ead418
Alex Watts <alexjwatts83@gmail.com>
2021-08-20 21:02:32 +1000

    Added functions startup

---
+++ src/MediatRDemo.Functions/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Reflection;
+
+[assembly: FunctionsStartup(typeof(MediatRDemo.Functions.Startup))]
+namespace MediatRDemo.Functions
+{
+	public class Startup : FunctionsStartup
+	{
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+			var configuration = new ConfigurationBuilder()
+				.AddUserSecrets(Assembly.GetExecutingAssembly(), false)
+				.AddEnvironmentVariables()
+				.Build();
+
+			builder
+				.Services
+				.Replace(ServiceDescriptor.Singleton(typeof(IConfiguration), configuration));
+		}
+	}
+}
